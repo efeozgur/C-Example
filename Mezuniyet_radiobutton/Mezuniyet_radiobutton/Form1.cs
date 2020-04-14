e75010c3491c1090f6c41000627bb5d15e3525d5
adaletinsesi15 <adaletinsesi15@gmail.com>
2020-04-14 23:54:24 +0300

    mezuniyet code

---
+++ Mezuniyet_radiobutton/Mezuniyet_radiobutton/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mezuniyet_radiobutton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                label2.Text = radioButton1.Text;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                label2.Text = radioButton2.Text;
+            }
+            else if (radioButton3.Checked == true)
+            {
+                label2.Text = radioButton3.Text;
+            }
+            else if (radioButton4.Checked == true)
+            {
+                label2.Text = radioButton4.Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
